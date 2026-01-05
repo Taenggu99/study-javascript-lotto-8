0dc696d6cba35cbfb7e6dea34fca5bae10ba2974
Taenggu99 <leetaeeun99@gmail.com>
2026-01-06 08:56:51 +0900

    프로젝트 기본설정

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+
+class App {
+  async run() {}
+}
+
+export default App;
---
+++ src/Lotto.js
@@ -0,0 +1,5 @@
+import { Random } from "@woowacourse/mission-utils";
+
+class Lotto {}
+
+export default Lotto;
